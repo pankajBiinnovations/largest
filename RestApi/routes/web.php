ad8d7653c0f656ab9128c164109d274630053499
Pankaj Singh <110153178+pankajBiinnovations@users.noreply.github.com>
2023-06-05 15:02:34 +0530

    change

--- RestApi/routes/web.php
+++ RestApi/routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    dd("hello");
-    // return view('welcome');
+    // dd("hello");
+    // // return view('welcome');
 });
