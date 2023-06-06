e359294446e4df9452c287ed553ff7b8585dc7fd
Pankaj Singh <110153178+pankajBiinnovations@users.noreply.github.com>
2023-06-06 20:35:11 +0530

    Update web.php

--- RestApi/routes/web.php
+++ RestApi/routes/web.php
@@ -9,7 +9,7 @@ use App\Http\controllers\EventController;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!!
+| be assigned to the "web" middleware group. Make something great!
 |+96551268397
 */
 
